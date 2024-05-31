700b2b5c27c7e763cf5111330a548f7308130528
Petteri Karjalainen <78467717+Ukkeli12@users.noreply.github.com>
2024-05-31 09:48:50 +0300

    Kommentteja ja muuta lisätty

--- Petterin koodit/Dense_koodi/dense.py
+++ Petterin koodit/Dense_koodi/dense.py
@@ -1,16 +1,32 @@
 import numpy as np
 
 
+"""
+Petteri Karjalainen OAMK 2024
+
+Calculating dense command from keras without keras library.
+
+output is: output = activation(dot(input, kernel) + bias) 
+
+activation is relu
+
+"""
+
+Batch_size = 2 
+Input_dim = 20
+
+
 class dense:
 
-    def relu_function(x): #Relu function in python
+    def relu_function(x): #Relu function in python it is 0.0 or higher
         return max(0.0, x)
     
-    test_array = np.array([1, 2, 3, 4, 5])
+    outputarray = np.empty(128, dtype=np.double)
     
-    
-np.array(200)
 
-    
-print(dense.test_array)   #Numpy array test
-print(dense.relu_function(90)) #Relu function test
+
+
+inputneurons = np.empty(128, dtype=np.double) #Empty numpy array for test
+
+inputneurons[0:2] = 20 #How to edit neurons data
+print(inputneurons) #Relu function test print
