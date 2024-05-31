faef32a32dfb5b68508a354ab881e5c5a1d4777f
Petteri Karjalainen <78467717+Ukkeli12@users.noreply.github.com>
2024-05-31 09:04:49 +0300

    Koodiin lisätty relu funktio ja muuta kokeilua

---
+++ Petterin koodit/Dense_koodi/dense.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+class dense:
+
+    def relu_function(x): #Relu function in python
+        return max(0.0, x)
+    
+    test_array = np.array([1, 2, 3, 4, 5])
+    
+    
+np.array(200)
+
+    
+print(dense.test_array)   #Numpy array test
+print(dense.relu_function(90)) #Relu function test
