0976559ff538c5d3e821d85419c12100cfcd113e
Petteri Karjalainen <78467717+Ukkeli12@users.noreply.github.com>
2024-05-31 13:41:44 +0300

    Dot function komento lisätty koodiin

--- Petterin koodit/Dense_koodi/dense.py
+++ Petterin koodit/Dense_koodi/dense.py
@@ -4,29 +4,28 @@ import numpy as np
 """
 Petteri Karjalainen OAMK 2024
 
-Calculating dense command from keras without keras library.
-
+Calculating dense function from keras without keras library.
 output is: output = activation(dot(input, kernel) + bias) 
-
-activation is relu
-
+activation function is relu
 """
 
-Batch_size = 2 
-Input_dim = 20
-
+Batch_size = 2 #Input batch size
+Input_dim = 20 #Input dim
 
 class dense:
 
     def relu_function(x): #Relu function in python it is 0.0 or higher
         return max(0.0, x)
     
+    def dot_function(a, b): #Calculating dot function 
+        return np.dot(a,b)
+    
     outputarray = np.empty(128, dtype=np.double)
     
+inputdata= np.empty(128, dtype=np.double) #Empty numpy array for test
+kerneldata = 200
 
+#inputneurons[0:2] = 20 #How to edit neurons data
+#print(inputneurons) #Relu function test print
 
-
-inputneurons = np.empty(128, dtype=np.double) #Empty numpy array for test
-
-inputneurons[0:2] = 20 #How to edit neurons data
-print(inputneurons) #Relu function test print
+print(dense.relu_function((dense.dot_function(inputdata, kernel) + bias)))
