99a5d13d6bec7c6ba7a50858244a70bf4d29d5c2
hsniemi <78792539+hsniemi@users.noreply.github.com>
2024-06-20 11:08:21 +0300

    dense 1 and 2 and parsing weights and biases from csv-files

--- dense.py
+++ dense.py
@@ -1,39 +1,79 @@
 import numpy as np  
 import random
-
-# Setting a random seed to allow better tracking
-np.random.seed(42)
+import csv
+import matplotlib.pyplot as plt
+import tensorflow as tf
 
 def relu_activation(x):
     return max(0, x)
 
-def generate_weights_and_bias(input_size, units):
-    weights = 0.1 * np.random.rand(input_size, units)
-    bias = np.zeros((1, units))
-    return weights, bias
+def get_weights(path_weights):
+    weights = []
+    with open(path_weights, newline='') as csvfile:
+        result = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+        for weight in result:
+            weights.append(weight)
+        weights = np.asarray(weights)
+    return weights
 
-# units specifies the number of neurons in the layer
+def get_biases(path_biases):
+    biases = []
+    with open(path_biases, newline='') as csvfile:
+        result = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+        for bias in result:
+            biases.append(bias[0])
+        biases = np.array(biases)
+    return biases
+
+def get_weights_and_biases(path_weights, path_biases):
+    weights = get_weights(path_weights)
+    biases = get_biases(path_biases)
+
+    return weights, biases
+
+# units specifies the number of neurons (outputs) in the layer
 def dense_1(input, units):
-    weights, bias = generate_weights_and_bias(len(input), units)
+    weights, bias = get_weights_and_biases('dense1Weights.csv', 'dense1Biases.csv')
+    
     output = []
-    for i in range(weights.shape[1]):
+    for i in range(units):
         product = 0
-        for j in range(weights.shape[0]):
+        for j in range(input.size):
             product += input[j] * weights[j][i] 
 
-        product += bias[0][i]
+        product += bias[i]
         product = relu_activation(product)
         output.append(product)
 
-    print("output ", output)
-    return np.array(output)
+    output = np.array(output)
+    print("dense1 output shape", output.shape)
+    return output
+
+def dense_2(input, units):
+    weights, bias = get_weights_and_biases('dense2Weights.csv', 'dense2Biases.csv')
+    
+    output = []
+    for i in range(units):
+        product = 0
+        for j in range(input.size):
+            product += input[j] * weights[j][i] 
+
+        product += bias[i]
+        output.append(product)
+
+    output = np.array(output)
+    print("dense2 output shape", output.shape)
+    return output
 
 def main():
-    input = np.array([0., 1., 2., 3., 
-                    3., 4., 5., 6.,
-                    5., 8., 9., 7.])
+    labels = ["Down", "Go", "Left", "No", "Right", "Stop", "Up", "Yes"]
+
+    input = np.random.rand(12544,)    
     
-    dense_1(input, 8)
+    dense1 = dense_1(input, 128)
+    predictions = dense_2(dense1, len(labels))
+    print("Predictions ", predictions)
+
 
 if __name__ == '__main__':
     main()
