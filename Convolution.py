89267959d9d36a4d9151d5e3683cd1e9eb4f4dfd
t0turi00 <79054967+t0turi00@users.noreply.github.com>
2024-07-08 13:25:41 +0300

    Fix readability issues in code and errors with too many outputs

--- Convolution.py
+++ Convolution.py
@@ -15,7 +15,6 @@ def convolution(image, kernel):
 
     kernel_size = len(kernel)
     row = image.shape[0] - len(kernel) + 1
-   
     col = image.shape[1] - len(kernel[0]) + 1
   
     
@@ -28,7 +27,6 @@ def convolution(image, kernel):
             multiplication = (sum(sum(current * kernel)))
             Output[i, j] = multiplication
     return Output
-
 
 def addNextImage(base, addition):
     try:
@@ -48,10 +46,10 @@ def addNextImage(base, addition):
 
 
 def applyAllFilters():
-    OutputCounter = 1
-    counter = 1
+    InputCounter = 1
+    FilterCounter = 1
     Channel = 1
-    channelCounter = 1
+    #channelCounter = 1
     Output = 1
     output_directory = f'Conv2_Output'
     #Saving the biases in an array for ease of use
@@ -72,17 +70,17 @@ def applyAllFilters():
         try:
             #Get conv1 outputs
             filterOutput = np.array([])
-            OutputFiles = f'./Conv1/Conv1_Output/filterOutput{OutputCounter}.csv' #Check the Output files location
+            Input = f'./Conv1/Conv1_Output/filterOutput{InputCounter}.csv' #Check the Output files location
             
            # Appending the rows in one filter to the empty filter array and then applying it on the image using the applySingleFilter function
-            with open(OutputFiles,newline='')as csvfile:
+            with open(Input,newline='')as csvfile:
                 reader = csv.reader(csvfile, delimiter=',')
                 for row in reader:
                     filterOutput = np.append(filterOutput, row)
                 filterOutput = filterOutput.reshape(30,30)
             
             filter = np.array([[]])
-            FilterFile = f'./filters_conv2/channel{channelCounter}_filter{counter}.csv'
+            FilterFile = f'./filters_conv2/channel{InputCounter}_filter{FilterCounter}.csv'
            # Appending the rows in one filter to the empty filter array and then applying it on the image using the applySingleFilter function
             with open(FilterFile,newline='')as csvfile:
                 csvReader = csv.reader(csvfile, delimiter=',',quoting=csv.QUOTE_NONNUMERIC)
@@ -93,41 +91,41 @@ def applyAllFilters():
             new_image = convolution(filterOutput, filter)
             
             #Copying image shape to Oimage to add all the other filters to it
-            if OutputCounter == 1:
-                Oimage = new_image.copy()
-                Oimage -= new_image
+            if InputCounter == 1:
+                Outputimage = new_image.copy()
+                Outputimage -= new_image
                 
-            Oimg = addNextImage(Oimage, new_image)
+            summedImage = addNextImage(Outputimage, new_image)
              
-            OutputCounter += 1
+            InputCounter += 1
                  
-            if(OutputCounter == 33):
+            if(InputCounter == 33):
 
                 # Adding the corresponding bias to all values in the filtered image and then calling the ReLu function on it
-                for row in range(Oimg.shape[0]):
-                    for column in range(Oimg.shape[1]):
-                        Oimg[row,column] += biases[counter-1]
+                for row in range(summedImage.shape[0]):
+                    for column in range(summedImage.shape[1]):
+                        summedImage[row,column] += biases[FilterCounter-1]
                         
                 
                 #ReLU        
-                OImage = ReLU(Oimg)
+                summedImage = ReLU(summedImage)
                 
-                counter += 1
+                FilterCounter += 1
                 
-                Oimage = np.array([])
-                np.savetxt(f'{output_directory}/Conv2Output{Output}.csv',OImage)
+                Outputimage = np.array([])
+                np.savetxt(f'{output_directory}/Conv2Output{Output}.csv',summedImage)
                 Output +=1
-                OImage = np.array([])
+                summedImage = np.array([])
         
-                OutputCounter = 1
+                InputCounter = 1
                 
-                if(counter == 65):
+                if(FilterCounter == 65):
                     channelCounter +=1
-                    counter = 1
+                    FilterCounter = 1
               
                 
         except:
-            print("Exit filter loop at channel: "+ str(channelCounter) + " & filter: " + str(counter))
+            print("Exit filter loop at channel: "+ str(InputCounter) + " & filter: " + str(FilterCounter))
             break
 
 #applyAllFilters()
